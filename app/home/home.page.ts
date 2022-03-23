c9353876ca83fef610ea8cfc3e96b8b84093769a
Ahmad Hamoush <hamoush@outlook.com>
2022-03-24 01:42:03 +0200

    First push with homepage

---
+++ app/home/home.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+
+})
+export class HomePage {
+
+  constructor(private route: Router) { }
+
+  login() {
+    this.route.navigate(['/login']);
+
+  }
+  public ionicNamedColor: string = 'primary';
+  animate(){
+    if(this.ionicNamedColor === 'primary') { 
+      this.ionicNamedColor = 'secondary'
+    } else {
+      this.ionicNamedColor = 'primary'
+    }
+  }
+
+}
+
