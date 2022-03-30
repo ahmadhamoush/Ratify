04c6abc1e552b3c716822db7c415062af2fbceba
Ahmad Hamoush <hamoush@outlook.com>
2022-03-30 19:52:15 +0300

    Added Sign Up page

--- app/login/login.page.ts
+++ app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
+
+  onClickSignup() {
+
+    // var bg = document.getElementById('bg');
+    // var hide = document.getElementById('hide');
+    // var show = document.getElementById('show');
+    // bg.style.top = '-20px';
+    // bg.style.height = '800px';
+    // bg.style.zIndex= '2';
+    // hide.style.display = 'none';
+    // show.style.display ="block";
+    this.route.navigate(['/sign-up']);
+}
 
   ngOnInit() {
   }
-
+  
+   
 }
